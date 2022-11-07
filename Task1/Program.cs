7ff6f7e56a7dacdfdd16503c23af3da7a3478bf0
Vitaliy Prukhnitskiy <Vitaliy152222@gmail.com>
2022-11-07 15:00:09 +0200

    Modified last 2 line of code

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,5 +2,5 @@
 Console.WriteLine("Мене звати");
 Console.WriteLine("Прухницький Віталій");
 Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Довільне повідомлення");
+Console.WriteLine("Anything");
