edf62c43413ea9419bb5a676111cdebc78954712
Vitaliy Prukhnitskiy <Vitaliy152222@gmail.com>
2022-11-07 14:58:28 +0200

    Modified first 3 line of code

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
+﻿Console.WriteLine("Привіт");
+Console.WriteLine("Мене звати");
+Console.WriteLine("Прухницький Віталій");
 Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
